3b673aa55c7acc4bb70015096fa11bc29d64b664
Sareh Heidari <sareh.heidari@gmail.com>
2019-01-17 10:01:25 +0000

    Fix collapsing tabs by moving event listeners to tabs

--- public/4.js
+++ public/4.js
@@ -1,58 +1,62 @@
   (function () {
   var tabInterfaces = document.querySelectorAll('.tabs');
+
+  function addOffscreenText(parent, child) {
+    var span = document.createElement('span');
+    var text = document.createTextNode('Current item, ');
+    span.setAttribute('id', 'current-item');
+    span.classList.add('gs-u-vh-heydon');
+    span.appendChild(text);
+    parent.insertBefore(span, child);
+  }
+
+  function removeOffscreenText() {
+    if (document.getElementById('current-item')) {
+      document.getElementById('current-item').remove();
+    }
+  }
+
   Array.prototype.forEach.call(tabInterfaces, function (tabInterface) {
-    // Save elements to memory
     var tablist = tabInterface.querySelector('ul');
     var tabs = tablist.querySelectorAll('a');
     var panels = tabInterface.querySelectorAll('section[id]');
     tablist.setAttribute('aria-label', 'options');
 
-    var switchTab = function (oldIndex, newIndex) {
-      if (oldIndex > -1) {
-        tabs[oldIndex].removeAttribute('class');
-        var span = document.getElementById('span');
-        tabs[oldIndex].removeChild(span);
-        panels[oldIndex].hidden = true;
-        panels[oldIndex].classList.add('hidden');
-      }
-
-      tabs[newIndex].setAttribute('class', 'selected-tab');
-
-      // tabs[newIndex].setAttribute('aria-current', 'true');
-
-      var span = document.createElement('span');
-      var spanString = 'Current item, ';
-      span.innerHTML = spanString;
-      span.setAttribute('id', 'span')
-      span.classList.add('gs-u-vh-heydon');
-      tabs[newIndex].insertBefore(span, tabs[newIndex].childNodes[0] || null);
-
-      panels[newIndex].hidden = false;
-      panels[newIndex].classList.remove('hidden');
-
-      //var p = panels[newIndex].getElementsByTagName('p');
-      //p[0].setAttribute('tabindex', '-1');
-      //panels[newIndex].focus();
-    }
-
     Array.prototype.forEach.call(tabs, function (tab, i) {
       tab.id = 'tab-'+tab.getAttribute('href').substring(1);
-
       var panel = panels[i];
-      //panel.setAttribute('tabindex', '-1');
-      //panel.setAttribute('aria-labelledby', tab.id);
-      //var p = panel.getElementsByTagName('p');
-      //p[0].setAttribute('tabindex', '-1');
       panel.hidden = true;
       panel.classList.add('hidden');
     });
 
-    tablist.addEventListener('click', function (e) {
-      var selected = tablist.querySelector('span').parentNode;
-      var oldIndex = selected ? Array.prototype.indexOf.call(tabs, selected) : undefined;
-      var newIndex = Array.prototype.indexOf.call(tabs, e.target);
-      switchTab(oldIndex, newIndex);
-    }, false);
+    var tabPanels = document.querySelectorAll('.tabs .panel');
+    var tabs = document.querySelectorAll('.tabs a');
+
+    function switchTabs(oldIndex, newIndex) {
+      tabPanels.item(oldIndex).hidden = true;
+      tabPanels.item(oldIndex).classList.add('hidden');
+      tabs.item(oldIndex).classList.remove('selected-tab');
+      tabPanels.item(newIndex).hidden = false;
+      tabPanels.item(newIndex).classList.remove('hidden');
+      tabs.item(newIndex).classList.add('selected-tab');
+      removeOffscreenText();
+      var newlySelectedTabLink = tabs.item(newIndex);
+      var newlySelectedTabLabel = newlySelectedTabLink.childNodes[0];
+      addOffscreenText(newlySelectedTabLink, newlySelectedTabLabel);
+    }
+
+    Array.prototype.map.call(tabs, function (tab, i) {
+      tab.addEventListener('click', function (e) {
+        if(e.target.classList.contains('selected-tab') === false) {
+          var oldTab = tablist.querySelector('a.selected-tab');
+          var oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
+          var newIndex = Array.prototype.indexOf.call(tabs, e.target);
+          switchTabs(oldIndex, newIndex);
+        } else {
+          e.preventDefault();
+        }
+      });
+    });
 
     window.addEventListener('DOMContentLoaded', function () {
       if (window.location.hash) {
@@ -61,7 +65,7 @@
           document.getElementById('tab-'+window.location.hash.substring(1))
         );
       }
-      switchTab(undefined, tabMatch > -1 ? tabMatch : 0);
+      switchTabs(0, tabMatch);
     });
   });
 })();
